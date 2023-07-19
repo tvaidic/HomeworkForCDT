45e339a7f74279de9a35f7c0a3fa1e86dac31d57
tvaidic <108301349+tvaidic@users.noreply.github.com>
2023-07-19 17:10:32 -0500

    Add files via upload

---
+++ sizingmod.py
@@ -0,0 +1,9 @@
+def square_footage(length: int,width: int):
+    return length * width
+
+def calculate_circumfrance(radius: int):
+    from math import pi
+    return 2 * pi * radius
+
+def ft_to_in(ft: int):
+    return ft * 12
